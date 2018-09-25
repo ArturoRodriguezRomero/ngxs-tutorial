0ca8b29a65c6f4edf6ff01845fc44617e04a9d3d
Arturo <arturo.rodriguez@macbook-arturorodriguez.local>
2018-09-25 12:57:52 +0200

    First Commit!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { Increment, Decrement, SetTotal } from './store/counter.actions';
+import { Observable } from 'rxjs';
+import { CounterStateModel, CounterState } from './store/counter.state';
+import { UsersRequestAttempt } from './store/users.actions';
+import { UsersStateModel } from './store/users.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngxs-tutorial';
+export class AppComponent implements OnInit {
+  // Seleccionamos el 'slice' counter del estado global.
+  @Select(state => state.counter)
+  counter$: Observable<CounterStateModel>;
+
+  @Select(state => state.users)
+  users$: Observable<UsersStateModel>;
+
+  // Injectamos la store global en el componente.
+  constructor(private store: Store) {}
+
+  // Invocamos a las acciones del store.
+  increment() {
+    this.store.dispatch(new Increment());
+  }
+
+  decrement() {
+    this.store.dispatch(new Decrement());
+  }
+
+  reset() {
+    this.store.dispatch(new SetTotal(0));
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(new UsersRequestAttempt());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgxsModule } from '@ngxs/store';
+
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { CounterState } from './store/counter.state';
+import { UsersState } from './store/users.state';
+import { HttpClientModule } from '@angular/common/http';
+
+export function noop() {
+  return function() {};
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([CounterState, UsersState], { developmentMode: true }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
